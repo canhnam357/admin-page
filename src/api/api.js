010dbdee9451a629612fdb40ded19df567f2fc8b
Canh Nam <canhnam357@gmail.com>
2025-05-13 18:10:32 +0700

    change url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://api.erotskoob.xyz/api",
 });
 
 // Hàm để cấu hình interceptor với store (chỉ thêm token vào header)
