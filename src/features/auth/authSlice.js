3ec47a203ba2facf0c32370ebc825878f0caa457
Canh Nam <canhnam357@gmail.com>
2025-05-25 21:42:56 +0700

    403-401

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -2,6 +2,23 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import api from '../../api/api';
 import { toast } from 'react-toastify';
 
+const decodeJWT = (token) => {
+    try {
+        const base64Url = token.split('.')[1]; // Lấy phần Payload
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/'); // Thay thế ký tự
+        const jsonPayload = decodeURIComponent(
+            atob(base64)
+                .split('')
+                .map((c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+                .join('')
+        );
+        return JSON.parse(jsonPayload); // Trả về object payload
+    } catch (error) {
+        console.error('Lỗi giải mã JWT:', error);
+        return null;
+    }
+};
+
 export const loginUser = createAsyncThunk(
   'auth/loginUser',
   async ({ email, password }, { rejectWithValue }) => {
@@ -11,6 +28,12 @@ export const loginUser = createAsyncThunk(
         const { accessToken, refreshToken, username } = response.data.result;
         localStorage.setItem('accessToken', accessToken);
         localStorage.setItem('refreshToken', refreshToken);
+        const decode = decodeJWT(accessToken);
+        if (decode.user_role !== "ADMIN") {
+          const message = 'Bạn không có quyền truy cập';
+          const statusCode = response?.data?.statusCode;
+          return rejectWithValue({message, statusCode});
+        }
         return { username };
       }
       throw new Error(response.data.message || 'Đăng nhập thất bại!');
